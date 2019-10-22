282370c98dbadf234912522679849a052fd6b7fe
Claro Alvino <cas3@ecomp.poli.br>
2019-10-22 20:07:06 -0300

    Update aeromocas.py
    falta salvar a lista de max distancia e retornar a min entre elas

--- aeromocas.py
+++ aeromocas.py
@@ -1,29 +1,22 @@
-# Python program for Dijkstra's single
-# source shortest path algorithm. The program is
-# for adjacency matrix representation of the graph
 class Graph():
 
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
-        # print('graph initilaized:', self.graph)
+        self.graph = [[0] * (vertices) for i in range(vertices)]
+
 
     def printSolution(self, dist, src):
         print("Vertex to Distance from Source")
         for node in range(self.V):
             print(src, " to destination ", node, "'s distance is", dist[node])
 
-    # A utility function to find the vertex with
-    # minimum distance value, from the set of vertices
-    # not yet included in shortest path tree
+
     def minDistance(self, dist, sptSet):
 
-        # Initilaize minimum distance for next node
+
         min = float('inf')
 
-        # Search not nearest vertex not in the
-        # shortest path tree
+
         for v in range(self.V):
             if dist[v] < min and sptSet[v] == False:
                 min = dist[v]
@@ -31,9 +24,7 @@ class Graph():
 
         return min_index
 
-    # Funtion that implements Dijkstra's single source
-    # shortest path algorithm for a graph represented
-    # using adjacency matrix representation
+
     def dijkstra(self, src):
 
         self.dist = dist = [float('inf')] * self.V
@@ -42,19 +33,13 @@ class Graph():
 
         for cout in range(self.V):
 
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            # u is always equal to src in first iteration
+
             u = self.minDistance(dist, sptSet)
 
-            # Put the minimum distance vertex in the
-            # shotest path tree
+
             sptSet[u] = True
 
-            # Update dist value of the adjacent vertices
-            # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shotest path tree
+
             for v in range(self.V):
                 if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]:
                     dist[v] = dist[u] + self.graph[u][v]
@@ -62,41 +47,27 @@ class Graph():
         self.printSolution(dist, src)
 
 
-# Driver program
-g = Graph(9)
-# zero means there are no links to that node
-g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
-           [4, 0, 8, 0, 0, 0, 0, 11, 0],
-           [0, 8, 0, 7, 0, 4, 0, 0, 2],
-           [0, 0, 7, 0, 9, 14, 0, 0, 0],
-           [0, 0, 0, 9, 0, 10, 0, 0, 0],
-           [0, 0, 4, 14, 10, 0, 2, 0, 0],
-           [0, 0, 0, 0, 0, 2, 0, 1, 6],
-           [8, 11, 0, 0, 0, 0, 1, 0, 7],
-           [0, 0, 2, 0, 0, 0, 6, 7, 0]
-           ]
-
 
 lista_primeira_linha = list(map(int,input().split()))
 vertices = lista_primeira_linha[0]#N
-arestras = lista_primeira_linha[1]#M
-lista2 = []
-pesos = []
+arestas = lista_primeira_linha[1]#M
+g = Graph(vertices)
+g.graph = [[0] * (vertices) for i in range(vertices)]
 
-for i in range(0,arestras): #salva os inputs numa lista [0]=cidade1, [1]=cidade2, [2]=peso entre as 2
-    lista2.append(list(map(int, input().split())))
-    pesos.append(lista2[i][2])
+for i in range(arestas):
+    inicio, fim, peso = [int(n) for n in input().split()]
+    g.graph[inicio][fim] = peso
+    g.graph[fim][inicio] = peso
 
-
-#x = int(len(g.graph))
-#lista = []
-#for i in range(x):
- #g.dijkstra(i)
- #lista.extend(g.dist)
-# print(g.dist)
+distancias = []
 
 
-print(vertices)
-print(arestras)
-print(lista2)
-print(pesos)
+for i in range(0,vertices):
+    g.dijkstra(i)
+    distancias.append(g.dist)
+
+#print(distancias)
+#distancias.sort
+#print(distancias)
+
+
