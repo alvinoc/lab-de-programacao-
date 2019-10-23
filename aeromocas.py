c62a7161724502a22569ea7669e746dfe49476ff
Claro Alvino <cas3@ecomp.poli.br>
2019-10-23 08:41:58 -0300

    Update aeromocas.py

--- aeromocas.py
+++ aeromocas.py
@@ -5,15 +5,7 @@ class Graph():
         self.graph = [[0] * (vertices) for i in range(vertices)]
 
 
-    def printSolution(self, dist, src):
-        print("Vertex to Distance from Source")
-        for node in range(self.V):
-            print(src, " to destination ", node, "'s distance is", dist[node])
-
-
     def minDistance(self, dist, sptSet):
-
-
         min = float('inf')
 
 
@@ -44,7 +36,7 @@ class Graph():
                 if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]:
                     dist[v] = dist[u] + self.graph[u][v]
 
-        self.printSolution(dist, src)
+
 
 def maximo(lista):
 
@@ -81,13 +73,14 @@ for i in range(0,vertices):
     g.dijkstra(i)
     distancias.append(g.dist)
 
-l = []
+dist_max = []
 for sublista in distancias:
-    l.append(maximo(sublista))
+    dist_max.append(maximo(sublista))
 
-
-print(distancias)
-print(l)
+min(dist_max)
+#print(distancias)
+#print(dist_max)
+print(min(dist_max))
 #print(sublista)
 #distancias.sort
 #print(distancias)
